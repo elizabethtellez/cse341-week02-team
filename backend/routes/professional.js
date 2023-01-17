cdf94b50b04f0165b681b065f379650a20a7d8b0
elizabethtellez <tel21004@byui.edu>
2023-01-17 13:30:29 -0800

    week 2 team assignment
    Co-authored-by: owamamwen1 <owamamwen1@users.noreply.github.com>

---
+++ backend/routes/professional.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const professionalController = require('../controllers/professional.js');
+
+const router = express.Router();
+
+// GET
+// localhost:8080/professional
+router.get('/', professionalController.getData);
+module.exports = router;
